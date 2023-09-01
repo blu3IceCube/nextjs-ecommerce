7e7543324a2f30c9585a35631c70ab54777ac24b
Snehil Shrivastava <snehilshrivastava3@gmail.com>
2023-09-01 21:35:56 +0530

    billboard data table

---
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/component/cell-action.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { BillboardColumn } from "./colums";
+
+interface CellActionProps {
+  data: BillboardColumn;
+}
+
+export const CellAction: React.FC<CellActionProps> = ({ data }) => {
+  return <div>Actions</div>;
+};
