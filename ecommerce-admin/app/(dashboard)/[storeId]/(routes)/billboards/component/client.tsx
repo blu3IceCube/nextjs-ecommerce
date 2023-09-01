4a7128a0f27993c4c634d62c6e09d53d16bb502e
Snehil Shrivastava <snehilshrivastava3@gmail.com>
2023-09-01 15:11:45 +0530

    adding billboards data table

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/component/client.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/component/client.tsx
@@ -3,12 +3,13 @@
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
-import { Billboard } from "@prisma/client";
 import { Plus } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
+import { BillboardColumn, columns } from "./colums";
+import { DataTable } from "@/components/ui/data-table";
 
 interface BIllboardClientProps {
-  data: Billboard[];
+  data: BillboardColumn[];
 }
 
 export const BillboardClient: React.FC<BIllboardClientProps> = ({ data }) => {
@@ -30,6 +31,7 @@ export const BillboardClient: React.FC<BIllboardClientProps> = ({ data }) => {
         </Button>
       </div>
       <Separator />
+      <DataTable columns={columns} data={data} />
     </>
   );
 };
--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,5 +1,7 @@
 import prismadb from "@/lib/prismadb";
+import { format } from "date-fns";
 import { BillboardClient } from "./component/client";
+import { BillboardColumn } from "./component/colums";
 
 const BillboardPage = async ({ params }: { params: { storeId: string } }) => {
   const billboards = await prismadb.billboard.findMany({
@@ -10,10 +12,17 @@ const BillboardPage = async ({ params }: { params: { storeId: string } }) => {
       createdAt: "desc",
     },
   });
+
+  const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
+    id: item.id,
+    label: item.label,
+    createdAt: format(item.createdAt, "MMMM do, yyyy"),
+  }));
+
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 py-6">
-        <BillboardClient data={billboards} />
+        <BillboardClient data={formattedBillboards} />
       </div>
     </div>
   );
