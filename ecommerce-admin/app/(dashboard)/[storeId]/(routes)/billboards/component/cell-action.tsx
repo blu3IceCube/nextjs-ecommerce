f43836456021b9ca4751bbe50209de1058ace0ae
Snehil Shrivastava <snehilshrivastava3@gmail.com>
2023-09-02 18:16:28 +0530

    adding cell actions

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/component/cell-action.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/component/cell-action.tsx
@@ -70,7 +70,7 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
           </DropdownMenuItem>
           <DropdownMenuItem
             onClick={() =>
-              router.push(`/${params.storeId}/billboard/${data.id}`)
+              router.push(`/${params.storeId}/billboards/${data.id}`)
             }
           >
             <Edit className="mr-2 h-4 w-4" />
