f8183756c9185cbe1841463aa275c1b8c6a55121
Snehil Shrivastava <snehilshrivastava3@gmail.com>
2023-09-02 18:53:02 +0530

    making nav sticky

--- ecommerce-admin/components/navbar.tsx
+++ ecommerce-admin/components/navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = async () => {
   });
 
   return (
-    <div className="border-b">
+    <div className="border-b sticky bg-slate-200 z-500">
       <div className="flex h-16 items-center px-4">
         <StoreSwitcher items={stores} />
         <MainNav className="mx-6" />
