3af03c03ce1829861b5dcc93cab1ccf7da27ec3f
Snehil Shrivastava <snehilshrivastava3@gmail.com>
2023-10-09 13:46:07 +0530

    adding sizes entity action

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/sizes/component/cell-action.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/sizes/component/cell-action.tsx
@@ -28,19 +28,17 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
 
   const onCopy = (id: string) => {
     navigator.clipboard.writeText(id);
-    toast.success("Billboard Id copied to the clipboard.");
+    toast.success("Size Id copied to the clipboard.");
   };
 
   const onDelete = async () => {
     try {
       setLoading(true);
-      await axios.delete(`/api/${params.storeId}/billboards/${data.id}`);
+      await axios.delete(`/api/${params.storeId}/sizes/${data.id}`);
       router.refresh();
-      toast.success("Billboard deleted");
+      toast.success("Size deleted");
     } catch (error) {
-      toast.error(
-        "Make sure you removed all categories using this billboard first."
-      );
+      toast.error("Make sure you removed all produxts using this sizes first.");
     } finally {
       setLoading(false);
       setOpen(false);
@@ -69,9 +67,7 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
             Copy Id
           </DropdownMenuItem>
           <DropdownMenuItem
-            onClick={() =>
-              router.push(`/${params.storeId}/billboards/${data.id}`)
-            }
+            onClick={() => router.push(`/${params.storeId}/sizes/${data.id}`)}
           >
             <Edit className="mr-2 h-4 w-4" />
             Update
